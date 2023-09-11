f0e3570394980748969ed5559da98f06fd4fc7da
jony <jony.rpm@gmail.com>
2023-09-11 11:27:21 -0300

    uno

--- inicio.js
+++ inicio.js
@@ -1,5 +1,9 @@
 let lista = document.getElementById("productos");
 
+document.addEventListener("DOMContentLoaded", () => {
+    generarCardsProductos(productosDisponibles)
+})
+
 fetch("./productos.json")
 .then((response) => response.json ())
 .then((data) => {
@@ -10,7 +14,24 @@ fetch("./productos.json")
         <img src= "${item.imagen}"/>
         <p>nombre: ${item.nombre}</p>
         <b>precio: ${item.precio}</b>
+        <button id="comprar${id}" class="btn btn-primary">añadir al carrito</button>
         `
-        listado.append(li)
+        listado.appendchild(li)
+
+        const btnComprar = document.getElementById(`comprar${id}`)
+            btnComprar.addEventListener("click", () => {
+               //alert de libreira
+                Swal.fire({
+                    title: 'Selecionado',
+                    text: 'Producto Añadido al Carrito',
+                    icon: 'success',
+                    confirmButtonText: 'Aceptar'
+                })
+                comprarProducto(id)
+            });
+
+
+
+
     });
 })
